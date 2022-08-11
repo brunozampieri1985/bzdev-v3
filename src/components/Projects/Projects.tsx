552b35bf7d1f2b02d0ebb6c845e6fdafb3fd640a
Bruno Zampieri <brunozampieri1985@gmail.com>
2022-08-11 11:32:09 -0300

    Section changes

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,7 +1,8 @@
+import { useInView } from 'react-intersection-observer'
+import { FaTools } from 'react-icons/fa'
 import styles from './Projects.module.css'
 import { useTheme } from '@contexts/ThemeProvider'
 import { useLanguage } from '@contexts/LanguageProvider'
-import { useInView } from 'react-intersection-observer'
 import data from './Projects.data'
 import SectionTitle from '@components/SectionTitle'
 import Slider from '@components/Slider'
@@ -15,7 +16,7 @@ const Projects: React.FC = () => {
   return (
     <section id="Projects" className={styles.projects}>
       <div ref={ref}>
-        {inView && <SectionTitle title={data.title[language]} />}
+        {inView && <SectionTitle title={data.title[language]} icon={FaTools} />}
       </div>
       <p>{data.description[language]}</p>
       <Slider data={data.projects} element={ProjectCard} />
--- src/components/SectionTitle/SectionTitle.tsx
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -1,25 +1,33 @@
 import styles from './SectionTitle.module.css'
 import { useTheme } from '@contexts/ThemeProvider'
+import { createElement } from 'react'
 
 type SectionTitleProps = {
   title: string
+  icon: React.ComponentType
 }
 
-const SectionTitle: React.FC<SectionTitleProps> = ({ title }) => {
+const SectionTitle: React.FC<SectionTitleProps> = ({ title, icon }) => {
   const { theme, thm } = useTheme()
 
   return (
-    <h1
-      style={
-        {
-          '--clr': thm.primary,
-          '--backClr': theme.mode === 'dark' ? '#1e1e1e' : '#fff',
-        } as React.CSSProperties
-      }
-      data-text={title}
-      className={styles.title}>
-      {title}
-    </h1>
+    <div className={styles.title}>
+      <h1
+        style={
+          {
+            '--clr': thm.primary,
+            '--backClr': theme.mode === 'dark' ? '#1e1e1e' : '#fff',
+            '--icon': thm.text
+          } as React.CSSProperties
+        }
+        data-text={title}
+        >
+        {title}
+        <span>
+        {createElement(icon)}
+        </span>
+      </h1>
+    </div>
   )
 }
 
